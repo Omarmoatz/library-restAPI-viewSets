71c836b8197f2212da8f8260b2b6624a03dcdab2
Omarmoatz <omar.raouf.002@gmail.com>
2024-02-29 01:54:27 +0200

    add mixins with view set

--- viewSets/urls.py
+++ viewSets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 
-from .views import BookViewSet
+from .views import BookViewSet,BookMixinViewSet
 
 router = DefaultRouter()
 router.register('viewSets', BookViewSet)
+router.register('viewSets/mixins', BookMixinViewSet,basename='book-mixins')
 
 urlpatterns = [
     path('', include(router.urls)),
--- viewSets/views.py
+++ viewSets/views.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,mixins
 
 from .models import Book
 from .serializers import BookSerilizer
 
 class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerilizer
+
+class BookMixinViewSet(mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin,
+                        viewsets.GenericViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerilizer
