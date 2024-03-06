56a4a8e0a9bdf85a805a7bb473a7fc8f2e725b66
Omarmoatz <omar.raouf.002@gmail.com>
2024-03-06 17:09:25 +0200

    add url for detail in the serializer

--- viewSets/serializers.py
+++ viewSets/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerilizer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    url = serializers.HyperlinkedIdentityField(
+        view_name= 'book_detail',
+        lookup_field= 'pk'
+    )
     class Meta:
         model = Book
         fields = '__all__'
--- viewSets/urls.py
+++ viewSets/urls.py
@@ -4,6 +4,6 @@ from .views import book_list,book_detail
 
 #  book/
 urlpatterns = [
-    path('list/', book_list),
-    path('list/<int:pk>/', book_detail),
+    path('list/', book_list , name='book_list'), 
+    path('list/<int:pk>/', book_detail, name='book_detail'),
 ]
