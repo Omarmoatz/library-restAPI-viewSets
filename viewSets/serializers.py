68f0f3b0f3c06b8996534003d2422c68926ec044
Omarmoatz <omar.raouf.002@gmail.com>
2024-04-17 16:34:23 +0200

    another way to add url to serializer

--- viewSets/serializers.py
+++ viewSets/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers 
+from rest_framework.reverse import reverse
 
 from .models import Book
 
 class BookSerilizer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
+    edite_url = serializers.SerializerMethodField(method_name='get_url')
     url = serializers.HyperlinkedIdentityField(
         view_name= 'book_detail',
         lookup_field= 'pk'
@@ -11,3 +13,9 @@ class BookSerilizer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def get_url(self,obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse('book_update' ,kwargs={'pk':obj.pk} ,request=request)
