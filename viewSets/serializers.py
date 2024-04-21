ea5ad30f1e6c416722e767b86b19a991552841c8
Omarmoatz <omar.raouf.002@gmail.com>
2024-04-21 16:23:49 +0200

    used another way for unique validation

--- viewSets/serializers.py
+++ viewSets/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from .models import Book
-from .validators import validate_title
+from . import validators
 
 class BookSerilizer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
@@ -12,22 +12,26 @@ class BookSerilizer(serializers.ModelSerializer):
         lookup_field= 'pk'
     )
     email = serializers.EmailField(write_only=True)
-    title = serializers.CharField(validators=[validate_title])
+    title = serializers.CharField(validators=[validators.unique_title, validators.validate_no_test])
+    
     class Meta:
         model = Book
         fields = '__all__'
 
     # def validate_field name()
-    # def validate_title(self,value):
-    #     qs = Book.objects.filter(title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(f'{value} this book is already exists')
-    #     return value
+
+    def validate_title(self,value):             # I should use the validator in the serializer 
+        request = self.context.get('request')   #   if i wanted to use the request like this example 
+        user = request.user                                
+        qs = Book.objects.filter( user=user ,title__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError(f'{value} this book is already exists')
+        return value
 
 
     def create(self, validated_data):
         email = validated_data.pop('email')         # you should do any edite in the serializer
-                                                    #  rather than in the view if you can
+                                                    #   rather than in the view if you can
         item = super().create(validated_data)
         # send email to someone
         return item
--- viewSets/validators.py
+++ viewSets/validators.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import Book
 
-def validate_title(value):
-    qs = Book.objects.filter(title__iexact=value)
-    if qs.exists():
-        raise serializers.ValidationError(f'{value} this book is already exists')
-    return value
+# def validate_title(value):
+#     qs = Book.objects.filter(title__iexact=value)
+#     if qs.exists():
+#         raise serializers.ValidationError(f'{value} this book is already exists')
+#     return value
+
+def validate_no_test(value):
+    if 'test' == value.lower():
+        raise serializers.ValidationError('the title must not contain test word ')
+    return
+
+unique_title = UniqueValidator(queryset=Book.objects.all())
